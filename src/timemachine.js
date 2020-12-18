584b0c34068681b4aad6f56b413907ffbd92ce1f
Shun Ueda <62182668+Kotlia@users.noreply.github.com>
2020-12-18 18:23:59 -0500

    Create table

--- src/timemachine.js
+++ src/timemachine.js
@@ -5,4 +5,23 @@ import { globalOption } from "./globalOption.js";
 
 export default class Timemachine {
     static globaloption = globalOption
+    constructor(param, meta) {
+        this.db = Database(Timemachine.globaloption.location)
+
+        this.filename = meta.url
+            .split("/")
+            .splice(-1)[0]
+            .toString()
+            .replace(".js", "")
+            .replace(".mjs", "")
+
+        this.db.exec(`
+            CREATE TABLE IF NOT EXISTS "${this.filename}"
+            (
+                "name"  TEXT NOT NULL UNIQUE,
+                "value" BLOB
+            );
+        `)
+
+    }
 }
